115f8f876fe9f3825347a3a3336603a229809eb7
Mael Veron <mael.veron@scalian.com>
2018-11-21 11:13:51 +0100

    Suprimer ctegorie OK, par contre modifier Origami n'apparait plus...

--- src/main/java/projet/lafactory/dao/IDAOCategorie.java
+++ src/main/java/projet/lafactory/dao/IDAOCategorie.java
@@ -1,8 +1,10 @@
 package projet.lafactory.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import projet.lafactory.model.Admin;
 import projet.lafactory.origami.Categorie;
@@ -15,6 +17,8 @@ public interface IDAOCategorie extends JpaRepository<Categorie, Integer> {
 	@Query("Select a from Categorie a where a.id = :id")
 	public Categorie findByLongId(@Param("id") long id);
 	
-	@Query("Delete a from Categorie a where a.id = :id")
+	@Transactional
+	@Modifying
+	@Query("Delete from Categorie a where a.id = :id")
 	public void deleteByLongId(@Param("id") long id);
 }
